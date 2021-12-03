05d8c2e55a6156254894697ef738726c5d52252c
Patrizio Fanelli <p.fanelli@joiasoftware.com>
2021-12-03 10:06:05 +0100

    modifiche a main

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const port = 3000;
 // TODO somet
 
 app.get('/', (req, res) => {
+  //console.log(res);
   res.send('Hello World!');
 });
 
+app.get('/admin', (req, res) => {
+  //console.log(test);
+  res.sendFile(__dirname+'/index.html');
+});
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log('Example app listening at http://localhost:${port}');
 });
